4fdb58a720443146db8a9cb64f18643311cc6578
xyd945 <ddisagenius@gmail.com>
2019-07-28 17:37:27 +0200

    deleted test.js

--- Demo/test_nodejs.py
+++
@@ -1,8 +0,0 @@
-from Naked.toolshed.shell import execute_js, muterun_js
-
-result = execute_js('device_management.js')
-
-if result:
-    print ("JavaScript is successfully executed")
-else:
-    print ("JavaScript is failed")
